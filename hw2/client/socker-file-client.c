17da56788233e15538cb070ed184c08257b58a4b
YunSeungHoe <dbstmdghl@naver.com>
2022-10-10 13:14:31 +0900

    update

--- hw2/client/socker-file-client.c
+++ hw2/client/socker-file-client.c
@@ -23,7 +23,6 @@ int main(int argc, char *argv[])
         printf("Usage : %s <IP> <port> <file name>\n", argv[0]);
         exit(1);
     }
-
     // argv[3]로 입력한 파일의 크기를 계산하기 위한 loop 
     for(int i = 0; argv[3][i] != '\0'; i++)
         fname_len++;
@@ -43,27 +42,31 @@ int main(int argc, char *argv[])
 
     if(connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
         error_handling("connect() error!");
-    //연결
-    // printf("sizeof = %d\n", fname_len);
-    // 서버로 파일명의 크기를 전송
+    // 연결 시작
+    // 서버로 파일 이름의 크기를 먼저 전송한다.
     name_len[0] = (char)fname_len;
     write(sock, name_len, sizeof(char)+1);
-    // 서버로 파일명을 전송
+    // 서버로 파일명을 전송한다.
     write(sock, argv[3], fname_len+1);
-    // file size 저장
+    // 파일 안에있는 내용의 크기를 확인하여 저장한다. 
+    // 버퍼보다 큰 값을 보낼 경우를 대비하여 보낸 데이터의 크기를 비교하기 위해 사용한다.
     file_size = filesize(argv[3]);
-    printf( "file size=%d\n", filesize(argv[3]));
+    // 파일 안에 내용의 크기를 확인하는 디버깅용.
+    // printf( "file size=%d\n", filesize(argv[3]));
+    // while을 사용하여 파일안에 크기와 보낸 데이터 크기를 비교하여 
+    // 보낸 데이터 크기가 파일안에 크기보다 작은동안 반복한다.
     // 보낸 파일 내용의 크기가 파일 크기보다 작을 수 있기 때문.
     while(send_len < file_size){
         fread(message, sizeof(char), sizeof(message)-1, fp);
-
-        printf("message : %s\n", message);
+        // printf("message : %s\n", message);
         send_cnt = write(sock, message, strlen(message)+1);
+        // 값을 초기화 해준다.
         memset(&message, 0, sizeof(message));
-        printf("send_cnt = %d", send_cnt);
+        // printf("send_cnt = %d", send_cnt);
+        // 보낸 데이터의 값을 추가해준다. 
         send_len = send_len + send_cnt;
     }
-    // 해제
+    // 연결 해제
     close(sock);
     fclose(fp);
     return 0;
--- hw2/server/socker-file-server.c
+++ hw2/server/socker-file-server.c
@@ -43,36 +43,34 @@ int main(int argc, char *argv[])
     clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
     if(clnt_sock == -1)
         error_handling("accept() error");
-    //연결
+    //연결 시작 
+    //txt 파일의 파일명의 길이를 클라이언트로부터 받는다. 
     if(read(clnt_sock, name_len, sizeof(char)+1) == -1)
         error_handling("read() error!");
     // printf("len = %d", (int)name_len[0]);
-    // 파일명의 크기도 클라이언트로 부터 받아야한다.
-    // str_len = read(clnt_sock, file_name, 6);
+    // 클라이언트로 부터 받은 파일명의 길이를 이용해 파일의 이름을 받는다.
     if(read(clnt_sock, file_name, (int)name_len[0]+1) == -1)
         error_handling("read() error!");
-    printf("Message from client : %s \n", file_name);
+    // 클라이언트로부터 받은 파일의 이름을 확인하는 디버깅용
+    // printf("Message from client : %s \n", file_name);
     
     // 클라이언트로부터 받은 파일을 쓰기모드로 오픈 
     fp = fopen(file_name, "w");
     if(fp == NULL)
         error_handling("fopen() error!");
-
+    // 클라이언트로부터 받은 파일의 내용이 버퍼의 크기를 초과할 수 있기때문에
+    // 서버는 클라이언트가 write함수가 끝날때까지 받는다. 
     while((str_len=read(clnt_sock, message, BUF_SIZE)) != 0){
-        printf("rev : %s\n", message);
+        // 내용을 확인하는 디버깅용
+        // printf("rev : %s\n", message);
         fputs(message, fp);
     }
-            
-
     // 클라이언트가 전송하는 파일명의 길이를 확인하는 디버깅 용
     // int name_len = 0;
     // for(int i = 0; file_name[i] != '\0'; i++)
     //     name_len++;
     // printf("name size = %d", name_len);
-  
-  
-  
-    //해제
+    //연결 해제
     close(clnt_sock);
     close(serv_sock);
     fclose(fp);
