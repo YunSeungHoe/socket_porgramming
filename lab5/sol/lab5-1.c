dcae9aaa856fb80485e0ee2b46d09c42b9ed6629
YunSeungHoe <dbstmdghl@naver.com>
2022-11-24 15:49:17 +0900

    update

---
+++ lab5/sol/lab5-1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <pigpio.h>
+
+int main(){
+    int trig = 17;
+    int echo = 27;
+    unsigned int start, end;
+    float distance;
+
+    gpioInitialise();
+    gpioSetMode(trig, PI_OUTPUT);
+    gpioSetMode(echo, PI_INPUT);
+    gpioWrite(trig, 0);
+    gpioDelay(100000);
+
+    while(1){
+        gpioWrite(trig, 1);
+        gpioDelay(10);
+        gpioWrite(trig, 0);
+        while(gpioRead(echo) == 0)
+            start = gpioTick();
+        while(gpioRead(echo) == 1)
+            end = gpioTick();
+        
+        distance = (end - start) / 58.0f;
+        printf("diff: %u, distance (cm) : %f\n", \
+                end - start, distance);
+
+        gpioWrite(trig, 0);
+        gpioDelay(100000);
+    }
+    gpioTerminate();
+    return 0;
+}
